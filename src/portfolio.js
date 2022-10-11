32aac2b18e7a34251f433de34a893f2b27818f34
arjunarj <nagarjunan66@gmail.com>
2022-10-12 00:34:07 +0530

    Second commit

---
+++ src/portfolio.js
@@ -0,0 +1,70 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  // homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  title: 'ARJ.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Nagarjuan G',
+  role: 'Student/ Developer',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: '',
+  social: {
+    linkedin: 'https://linkedin.com/in/nagarjunan-g-545a98241',
+    github: 'https://github.com/arjunarj',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'dE-Vote',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['React', 'Javascript', 'Blockchain'],
+    sourceCode: 'https://github.com/arjunarj',
+    livePreview: '',
+  },
+  {
+    name: 'Nimmadhi',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['Flutter', 'Firebase', 'Dart'],
+    sourceCode: 'https://github.com/arjunarj',
+    livePreview: '',
+  },
+  {
+    name: 'Skanmed',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['Flutter', 'Firebase', 'Dart'],
+    sourceCode: 'https://github.com/arjunarj',
+    livePreview: '',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Python',
+  'Java',
+  'C',
+  'JavaScript',
+  'Flutter',
+  'React',
+  'MongoDB',
+  'Firebase',
+  'Heroku',
+  'Git'
+
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'nagarjunan66@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
